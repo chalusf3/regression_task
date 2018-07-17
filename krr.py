9720ca953883d3e4536da2b33beae4881200574a
François Chalus <chalusf3@gmail.com>
2018-07-17 15:15:04 +0100

    uncommented linreg

--- krr.py
+++ krr.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-"""
 def lin_reg(X, y, noise_var):
     # returns the coefficient vector beta for y = X * beta + epsilon
     # N(0,1) prior on beta and a N(0,I noise_var) on the noise
     beta = np.linalg.solve((noise_var * np.eye(X.shape[1]) + np.dot(X.T, X)), np.dot(X.T, y))
     return beta
 
+"""
 def fit_kernel(K_pred, K, y, noise_var):
     # returns the MLE fit of the x_pred under the model y = loc * beta + N(0, noise_var) with a N(0,1) prior on beta
     # K_pred[i,j] = k(x_pred_i, loc_j) (loc are the points used to create features / at training time)
